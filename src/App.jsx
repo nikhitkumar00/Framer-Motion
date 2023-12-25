69e50104cf34817a7284fe0d6141100f2072f67f
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-25 16:28:31 +0530

    changed the assets folder to images

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import Parallax1 from "./Components/Parallax1";
 
 function App() {
-  return (
-    // <div>
-    //   <div className="bold flex h-96 w-full items-center justify-center bg-black text-6xl text-white">
-    //     SCROLL DOWN
-    //   </div>
-      <Parallax1 />
-    //   {/* <div className="bold flex h-screen w-full items-center justify-center bg-black text-6xl text-white relative z-50">
-    //     SCROLL UP
-    //   </div>
-    // </div> */}
-  );
+  return <Parallax1 />;
 }
 
 export default App;
