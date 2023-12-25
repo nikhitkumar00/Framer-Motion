d345cf3df8a0b8a647a8f3c12a086c4067bfac9e
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-25 17:18:38 +0530

    image fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ export default {
     extend: {
       backgroundImage: {
         jungle1: "url('https://parallaxnikhit.netlify.app/images/jungle1.png')",
-        jungle2: "url('https://parallaxnikhit.netlify.apphttps://parallaxnikhit.netlify.app/images/jungle2.png')",
-        jungle3: "url('https://parallaxnikhit.netlify.apphttps://parallaxnikhit.netlify.app/images/jungle3.png')",
-        jungle4: "url('https://parallaxnikhit.netlify.apphttps://parallaxnikhit.netlify.app/images/jungle4.png')",
-        jungle5: "url('https://parallaxnikhit.netlify.apphttps://parallaxnikhit.netlify.app/images/jungle5.png')",
+        jungle2: "url('https://parallaxnikhit.netlify.app/images/jungle2.png')",
+        jungle3: "url('https://parallaxnikhit.netlify.app/images/jungle3.png')",
+        jungle4: "url('https://parallaxnikhit.netlify.app/images/jungle4.png')",
+        jungle5: "url('https://parallaxnikhit.netlify.app/images/jungle5.png')",
         logo: "url('https://parallaxnikhit.netlify.app/images/logo_land.png')",
         man: "url('https://parallaxnikhit.netlify.app/images/man_on_mountain.png')",
         mountain: "url('https://parallaxnikhit.netlify.app/images/mountains.png')",
