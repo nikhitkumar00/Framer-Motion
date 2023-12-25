9d7ce0e97075343b7f56db1a30bbc134bdec09e9
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-25 13:37:35 +0530

    configured framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Parallax1 from "./Components/Parallax1";
 function App() {
   return (
     <div>
+      <div className="bold flex h-96 w-full items-center justify-center bg-black text-6xl text-white">
+        SCROLL DOWN
+      </div>
       <Parallax1 />
+      <div className="bold flex h-96 w-full items-center justify-center bg-black text-6xl text-white">
+        SCROLL UP
+      </div>
     </div>
   );
 }
--- src/Components/Parallax1.jsx
+++ src/Components/Parallax1.jsx
@@ -2,16 +2,107 @@ import { useScroll, motion, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 function Parallax1() {
-    return (<div className="relative bg-slate-500 h-screen w-full flex justify-center items-center">
-        <div className="bg-jungle1 absolute inset-0"></div>
-        <div className="bg-jungle2 absolute inset-0"></div>
-        <div className="bg-jungle3 absolute inset-0"></div>
-        <div className="bg-jungle4 absolute inset-0"></div>
-        <div className="bg-jungle5 absolute inset-0"></div>
-        <div className="bg-logo absolute inset-0"></div>
-        <div className="bg-man absolute inset-0"></div>
-        <div className="bg-mountain absolute inset-0"></div>
-    </div> );
+  const refference = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: refference,
+    offset: ["start start", "end start"],
+  });
+
+  const background = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const jungle1 = useTransform(scrollYProgress, [0, 1], ["0%", "10%"]);
+  const jungle2 = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const jungle3 = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const jungle4 = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const jungle5 = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const man = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const mountain = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const logo = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div className="relative h-screen w-full" ref={refference}>
+      <motion.div
+        style={{
+          y: background,
+          backgroundImage: 'url("./src/assets/background.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 -z-50 h-screen w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: logo,
+          backgroundImage: 'url("./src/assets/logo_land.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 -z-40 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: mountain,
+          backgroundImage: 'url("./src/assets/mountains.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 -z-20 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: jungle1,
+          backgroundImage: 'url("./src/assets/jungle1.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-0 h-screen w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: jungle2,
+          backgroundImage: 'url("./src/assets/jungle2.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-10 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: jungle3,
+          backgroundImage: 'url("./src/assets/jungle3.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-20 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: jungle4,
+          backgroundImage: 'url("./src/assets/jungle4.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-30 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: jungle5,
+          backgroundImage: 'url("./src/assets/jungle5.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-40 w-full"
+      ></motion.div>
+      <motion.div
+        style={{
+          y: man,
+          backgroundImage: 'url("./src/assets/man_on_mountain.png")',
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="absolute inset-0 z-50 w-full"
+      ></motion.div>
+    </div>
+  );
 }
 
 export default Parallax1;
