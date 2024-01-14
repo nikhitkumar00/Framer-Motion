e9bbf96af603a21f3013c14bb75d11db4d65d5f9
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-14 10:12:18 +0530

    added loading timeout max limit

--- src/Components/LoadingPage.jsx
+++ src/Components/LoadingPage.jsx
@@ -9,15 +9,21 @@ const LoadingPage = () => {
     };
 
     window.addEventListener("load", handleLoad);
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
 
-    return () => window.removeEventListener("load", handleLoad);
+    return () => {
+      window.removeEventListener("load", handleLoad);
+      clearTimeout(timeoutId);
+    };
   }, []);
 
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 z-[9999] flex h-screen w-screen items-center justify-center bg-[#FFAF1B] text-6xl text-[#DE711D]">
-      <span className="animate-pulse">LoadingPage</span>
+    <div className="fixed inset-0 z-[9999] flex h-dvh w-screen items-center justify-center bg-[#FFAF1B] text-6xl text-[#DE711D]">
+      <span className="animate-pulse">Loading...</span>
     </div>
   );
 };
